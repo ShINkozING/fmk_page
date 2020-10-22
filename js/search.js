e11c97a4b19f92f9680bb1def1d1497025961eda
ShINkozING <sshinkyeah@gmail.com>
2020-10-22 20:00:45 +0500

    1

--- js/search.js
+++ js/search.js
@@ -1,7 +1,14 @@
 const imageInput = document.querySelector('#img-search')
 let inputSearch = document.querySelector('.header__right-input')
+let focusSearch = document.querySelector('#input-search')
 console.log(inputSearch);
 
-imageInput.addEventListener('click', ()=> {
+imageInput.addEventListener('click', () => {
     inputSearch.classList.add('is-active')
+    focusSearch.focus()
+    
 })
+
+if(focusSearch !== focusSearch.focus()) {
+    inputSearch.classList.remove('is-active')
+}
